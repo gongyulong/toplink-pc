6eb0f305991eb028d0006a5c30509079950512b2
gongyulong <2411752970@qq.com>
2019-10-08 17:37:46 +0800

    添加登录&主页路由

--- src/router.js
+++ src/router.js
@@ -2,17 +2,36 @@
 import Vue from 'vue'
 // 导入了第三方包 vue-router
 import Router from 'vue-router'
-// 导入 useElement
-import UseElement from './views/useElement/index'
+// 导入useElement
+import UseElement from '@/views/useElement'
+// 导入router && home
+import Login from '@/views/login'
+import Home from '@/views/home'
 
 // 使用路由
 Vue.use(Router)
 
 // 创建并且导出一个 Router 对象
 export default new Router({
-  routes: [{
-    name: 'useElement',
-    path: '/useElement',
-    component: UseElement
-  }]
+  routes: [
+    { path: '/useElement',
+      name: 'useElement',
+      component: UseElement
+    },
+    // 设置路由选项
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
+    }
+  ]
 })
---
+++ src/views/home/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+      <h3>我是 home主页</h3>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+<style>
+</style>
