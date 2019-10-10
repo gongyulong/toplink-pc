751786476073d23713eacb62a17a4ff50a852775
gongyulong <2411752970@qq.com>
2019-10-10 12:41:11 +0800

    创建layout路由&完成布局

--- src/router.js
+++ src/router.js
@@ -7,6 +7,10 @@ import UseElement from '@/views/useElement'
 // 导入router && home
 import Login from '@/views/login'
 import Home from '@/views/home'
+// 导入router && home
+import Layout from '@/views/layout'
+// 导入publish
+import Publish from '@/views/publish'
 
 // 使用路由
 Vue.use(Router)
@@ -14,24 +18,37 @@ Vue.use(Router)
 // 创建并且导出一个 Router 对象
 export default new Router({
   routes: [
-    { path: '/useElement',
-      name: 'useElement',
-      component: UseElement
-    },
     // 设置路由选项
     {
       path: '/',
       redirect: '/home'
     },
+    // 测试路由
+    { path: '/useElement',
+      name: 'useElement',
+      component: UseElement
+    },
+    // 登录路由
     {
       path: '/login',
       name: 'login',
       component: Login
     },
+    // 布局路由
     {
-      path: '/home',
-      name: 'home',
-      component: Home
+      path: '/layout',
+      name: 'layout',
+      component: Layout,
+      children: [
+        {
+          path: '/home',
+          component: Home
+        },
+        {
+          path: '/publish',
+          component: Publish
+        }
+      ]
     }
   ]
 })
---
+++ src/views/layout/components/layroutAsider.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="appasider">
+    <!--项目的 logo -->
+    <div class="logo">
+      <img src="./img/logo_admin.png" alt />
+    </div>
+    <!-- 导航菜单栏 -->
+    <el-menu :router="true" :unique-opened="true" default-active="2" class="el-menu-vertical-demo" background-color="#353b4e" text-color="#ccc" active-text-color="#409eff">
+      <el-menu-item index="1">
+        <span slot="title">首页</span>
+      </el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">
+          <span>内容管理</span>
+        </template>
+        <el-menu-item index="/publish">
+          <span slot="title">发布文章</span>
+        </el-menu-item>
+        <el-menu-item index="2-2">
+          <span slot="title">内容列表</span>
+        </el-menu-item>
+        <el-menu-item index="2-3">
+          <span slot="title">评论列表</span>
+        </el-menu-item>
+        <el-menu-item index="2-4">
+          <span slot="title">素材管理</span>
+        </el-menu-item>
+      </el-submenu>
+      <el-submenu index="3">
+        <template slot="title">
+          <span>粉丝管理</span>
+        </template>
+        <el-menu-item index="3-1">
+          <span slot="title">图文数据</span>
+        </el-menu-item>
+        <el-menu-item index="3-2">
+          <span slot="title">粉丝概况</span>
+        </el-menu-item>
+        <el-menu-item index="3-3">
+          <span slot="title">粉丝画像</span>
+        </el-menu-item>
+        <el-menu-item index="3-4">
+          <span slot="title">粉丝列表</span>
+        </el-menu-item>
+      </el-submenu>
+      <el-menu-item index="4">
+        <span slot="title">帐户信息</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
